e1fb490dbb43e3699f53b55a7beaf5f4acb2d6ba
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-06 21:22:28 +0530

    validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,12 +5,16 @@ const getAllTasks = (req, res) => {
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({task});
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
 }
 
 const getTask = (req, res) => {
-  res.json({id:req.params.id});
+  res.json({ id: req.params.id });
 }
 
 const updateTask = (req, res) => {
