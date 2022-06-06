fe2367c7ad3ba65635052590c15a6de847de6981
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-06 11:03:55 +0530

    .env file and db connection

---
+++ controllers/tasks.js
@@ -0,0 +1,30 @@
+
+
+const getAllTasks = (req, res) => {
+  res.send('All Items from the file')
+}
+
+const createTask = (req, res) => {
+  res.json(req.body);
+}
+
+const getTask = (req, res) => {
+  res.json({id:req.params.id});
+}
+
+const updateTask = (req, res) => {
+  res.send('Update Task');
+}
+const deleteTask = (req, res) => {
+  res.send('Delete Task');
+}
+
+
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+}
