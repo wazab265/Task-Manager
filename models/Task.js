d8c84954e22b0602705c3202743aa300c0324865
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-06 11:39:11 +0530

    Set up and implement Schema

--- models/Task.js
+++ models/Task.js
@@ -1,9 +1,10 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 
-const TaskSchema=new mongoose.Schema({
-  name:String,completed:Boolean
+const TaskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean
 })
 
-module.exports=mongoose.model('Task',TaskSchema)
+module.exports = mongoose.model('Task', TaskSchema)
 
