b2450ed75a7b36170aa02ba9edd65057b1558c28
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-08 10:41:25 +0530

    custom error class

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
+const {createCustomError}=require('../errors/custom-error')
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
@@ -10,14 +11,11 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task })
 })
 
-const getTask = asyncWrapper(async (req, res,next) => {
+const getTask = asyncWrapper(async (req, res,next) => { 
   const { id: taskID } = req.params
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    const error = new Error('Not Found');
-    error.status = 404
-    return next(error)
-    return res.status(404).json({ msg: `No task with id : ${taskID}` })
+    return next(createCustomError(`No task with id : ${taskID}`,404 ))
   }
   res.status(200).json({ task });
 })
@@ -27,7 +25,8 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` })
+    return next(createCustomError(`No task with id : ${taskID}`,404 ))
+
   }
   res.status(200).json({ task })
 })
@@ -39,7 +38,8 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   })
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` })
+    return next(createCustomError(`No task with id : ${taskID}`,404 ))
+
   }
   res.status(200).json({ task })
 })
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,9 @@
+const {CustomAPIError}=require('../errors/custom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
-  return res.status(err.status).json({ msg: err.message })
+  if (err instanceof CustomAPIError){
+    return res.status(err.statusCode).json({msg: err.message})
+  }
+  return res.status(err.status).json({ msg:'Something went wrong. Please try again.' })
 }
 module.exports =errorHandlerMiddleware
 
